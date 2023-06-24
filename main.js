6f6dbe2868478e0a2a46cfaa0405f970b6989aff
ninjacoder337 <87841586+ninjacoder337@users.noreply.github.com>
2023-06-24 12:48:14 -0400

    Add files via upload

---
+++ main.js
@@ -0,0 +1,44 @@
+noseX=0
+noseY=0
+
+function preload(){
+    dude_nose = loadImage("https://i.postimg.cc/3x3QzSGq/m.png");
+    
+}
+
+function setup()  {
+    canvas = createCanvas(300,300);
+    canvas.center();
+    video = createCapture(VIDEO);
+    video.size(300, 300);
+    video.hide();
+
+    poseNet = ml5.poseNet(video,modelLoaded);
+    poseNet.on("pose",gotPoses);
+}
+
+function modelLoaded() {
+    console.log("Posnet is Initialized");
+}
+
+function gotPoses(results) {
+    if(results.length > 0)
+     {
+        console.log(results);
+        noseX = results[0].pose.nose.x-15;
+        noseY = results[0].pose.nose.y-15;
+    }
+}
+
+function draw() {
+    image(video,0,0,300,300);
+    image(dude_nose, noseX, noseY, 30, 30);
+}
+
+
+
+
+
+function take_snapshot() {
+    save("myFilterImage.png");
+}
